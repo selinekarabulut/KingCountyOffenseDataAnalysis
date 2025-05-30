c2990d582e5ca5d70ac45c4d4f48b9d0c23fbc0e
Selin Karabulut, Ph.D. <114204917+selinekarabulut@users.noreply.github.com>
2025-05-29 18:32:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ def update_hourly_trend(selected_crime):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8050)
+   app.run_server(host='0.0.0.0', port=8050, debug=True)
